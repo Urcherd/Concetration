788f38c9f1d95efaee5dfb9190ed0ed45a72a7dc
Urcherd <kirikuchiha@gmail.com>
2018-04-03 10:18:03 +0300

    Initial Commit

---
+++ Concetration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concetration
+//
+//  Created by Kirill Kuzmichev on 03.04.18.
+//  Copyright © 2018 Kirill Kuzmichev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
